38e00bcc59e928eba0fd7c910ff0b3189d8400e7
Jeffrey Palermo <jeffrey@clear-measure.com>
2025-06-13 16:35:28 -0500

    Added a blazor login page and wired up authentication

---
+++ src/UI.Shared/Authentication/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,43 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace UI.Shared.Authentication
+{
+    public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private ClaimsPrincipal _currentUser = new ClaimsPrincipal(new ClaimsIdentity());
+        
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            return Task.FromResult(new AuthenticationState(_currentUser));
+        }
+
+        public void Login(string username)
+        {
+            var identity = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Name, username)
+            }, "Custom Authentication");
+            
+            _currentUser = new ClaimsPrincipal(identity);
+            
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+
+        public void Logout()
+        {
+            _currentUser = new ClaimsPrincipal(new ClaimsIdentity());
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+
+        public bool IsAuthenticated()
+        {
+            return _currentUser.Identity?.IsAuthenticated ?? false;
+        }
+
+        public string? GetUsername()
+        {
+            return _currentUser.Identity?.Name;
+        }
+    }
+}
--- src/UI/Client/Program.cs
+++ src/UI/Client/Program.cs
@@ -1,17 +1,18 @@
 using System.Collections;
 using System.Net.Http.Json;
 using BlazorApplicationInsights;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Logging.Abstractions;
 using Palermo.BlazorMvc;
 using ProgrammingWithPalermo.ChurchBulletin.Core;
 using UI.Client;
+using UI.Shared.Authentication;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
 
 var http = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
 var ConfigurationModel = await http.GetFromJsonAsync<ConfigurationModel>("Configuration");
@@ -24,10 +25,13 @@ if (ConfigurationModel != null)
     });
 }
 
+// Add authentication services
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<CustomAuthenticationStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(provider => 
+    provider.GetRequiredService<CustomAuthenticationStateProvider>());
 
 builder.Services.AddScoped<IUiBus>(provider => new MvcBus(NullLogger<MvcBus>.Instance));
 builder.Services.AddScoped(sp => http);
-
-
 
 await builder.Build().RunAsync();
