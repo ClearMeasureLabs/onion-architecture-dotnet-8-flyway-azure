d9bafc559a24c99fb3a7c7002aa7f3aeea1837da
Jeffrey Palermo <jeffrey@clear-measure.com>
2025-07-29 17:17:16 -0500

    unhooked json configuration at client startup

--- src/UI/Client/Program.cs
+++ src/UI/Client/Program.cs
@@ -1,44 +1,37 @@
-using System.Collections;
 using System.Net.Http.Json;
 using BlazorApplicationInsights;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Logging.Abstractions;
-using Palermo.BlazorMvc;
-using ProgrammingWithPalermo.ChurchBulletin.Core;
-using UI.Client;
-using UI.Shared.Authentication;
-using Core.Services;
 using Lamar;
 using Lamar.Microsoft.DependencyInjection;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using ProgrammingWithPalermo.ChurchBulletin.Core;
+using UI.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 var http = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
-var ConfigurationModel = await http.GetFromJsonAsync<ConfigurationModel>("Configuration");
 builder.Services.AddScoped(sp => http);
+ConfigurationModel? ConfigurationModel = new ConfigurationModel() { AppInsightsConnectionString = "" };//await http.GetFromJsonAsync<ConfigurationModel>("Configuration");}
 
 if (ConfigurationModel != null)
-{
     builder.Services.AddBlazorApplicationInsights(x =>
     {
         x.ConnectionString = ConfigurationModel.AppInsightsConnectionString;
     });
-}
 
 // Add authentication services
 builder.Services.AddAuthorizationCore();
 builder.ConfigureContainer<ServiceRegistry>(
-    new LamarServiceProviderFactory(), registry => 
+    new LamarServiceProviderFactory(), registry =>
         registry.IncludeRegistry<UIClientServiceRegistry>());
 
 
-string url = builder.Configuration.GetValue<string>("RemoteBusUrl") ?? throw new InvalidOperationException("Must have config value 'RemoteBusUrl'");
+var url = builder.Configuration.GetValue<string>("RemoteBusUrl") ??
+          throw new InvalidOperationException("Must have config value 'RemoteBusUrl'");
 builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(url) });
-WebAssemblyHost app = builder.Build();
+var app = builder.Build();
 await app.Services.GetRequiredService<HealthCheckService>().CheckHealthAsync();
 await app.RunAsync();
